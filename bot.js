53530fe5051a4c09da89c87c64ee0a8c0af3e452
Luffy12323 <41828478+Luffy12323@users.noreply.github.com>
2019-02-25 18:34:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,7 +158,7 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-  if (message.content.toLowerCase().startsWith(prefix+ `top server`))  {
+  if (message.content.toLowerCase().startsWith(prefix + `top server`))  {
 
 const top = client.guilds.sort((a,b)=>a.memberCount-b.memberCount).array().reverse()
 message.channel.send(`**Top 10 Servers : **\n1. **${top[0].name}**: ${top[0].memberCount} \n2. **${top[1].name}**: ${top[1].memberCount}.\n3. **${top[2].name}**: ${top[2].memberCount}.\n4. **${top[3].name}**: ${top[3].memberCount}.\n5. **${top[4].name}**: ${top[4].memberCount}.\n6. **${top[5].name}**: ${top[5].memberCount}.\n7. **${top[6].name}**: ${top[6].memberCount}.\n8. **${top[7].name}**: ${top[7].memberCount}.\n9. **${top[8].name}**: ${top[8].memberCount}.\n10. **${top[9].name}**: ${top[9].memberCount} .`)
