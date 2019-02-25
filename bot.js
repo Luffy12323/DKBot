5af7d62b08de40bf210216a26ec4227a7d6e7af6
Luffy12323 <41828478+Luffy12323@users.noreply.github.com>
2019-02-25 16:42:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,6 +49,35 @@ msg.delete();
 }
 });
 
+client.on('message', message => {//new msg event
+if(!message.channel.guild) return;
+  if(message.content.startsWith(prefix + 'Rainbow')) {//to create the rainbow role
+      let role = message.guild.roles.find('name', 'Rainbow Quick.')
+    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+  //start of create role 
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "Rainbow Quick.",//the role will create name
+   color: "#000000",//the default color
+   permissions:[]//the permissions
+ //end of create role
+})
+
+}
+message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+}})
+
+client.on('ready', () => {//new ready event
+  setInterval(function(){
+      client.guilds.forEach(g => {
+                  var role = g.roles.find('name', 'Rainbow Quick.');//rainbow role name
+                  if (role) {
+                      role.edit({color : "RANDOM"});
+                  };
+      });
+  }, 5000);//the rainbow time
+})
+
 
 client.on('message', message => {
        if (message.content.startsWith(prefix + 'botserver')) {
