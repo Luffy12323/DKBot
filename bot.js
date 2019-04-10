dcc17ef1a4d53f027271c00808fc11926b30bd97
Luffy12323 <41828478+Luffy12323@users.noreply.github.com>
2019-04-10 21:04:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -211,28 +211,6 @@ client.on('guildCreate', guild => {
 
 
 
-client.on('message', omar => {
-var prefix = "-";
-if(omar.content.split(' ')[0] == prefix + 'dc') {  
-if (!omar.channel.guild) return;
-if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
-if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
-omar.guild.channels.forEach(m => {
-m.delete();
-});
-}
-if(omar.content.split(' ')[0] == prefix + 'dr') { 
-if (!omar.channel.guild) return;
-if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**You Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-if(!omar.guild.member(client.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**I Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-omar.guild.roles.forEach(m => {
-m.delete();
-});
-omar.reply("`تم حذف جميع الرتب بنجاح`")
-}
-});
-
-
 
 client.on('message', message => {
 
